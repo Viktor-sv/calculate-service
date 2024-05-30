c258cb7870a63dd9b2f112c8abb3bfcb25deb896
unknown <viktor.stegachov@gmail.com>
2024-05-30 11:20:45 +0300

    minor fix

--- pkg/server/handler/middleware/validate.go
+++ pkg/server/handler/middleware/validate.go
@@ -1,18 +1,28 @@
 package middleware
 
 import (
+	"bytes"
+	"calculate-service/pkg/errors"
+	"encoding/json"
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"net/http"
 )
 
 func Validate(h httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		/*_, err := utils.ParseBody(r.Body)
+		bodyBytes, err := io.ReadAll(r.Body)
+		r.Body.Close() //  must close
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
+		var b []byte
+		_ = json.Unmarshal(bodyBytes, &b)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, errors.ResponseError(errors.IncorrectInputError))
 			return
-		}*/
+		}
 		h(w, r, ps)
 	}
 }
